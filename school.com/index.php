ca378955ddca6dfb9677a2d7b497818feeb30e1f
fabiano <fabiano@fabiano-PC>
2018-05-01 21:33:38 -0300

    Firt Commit

---
+++ school.com/index.php
@@ -0,0 +1,9 @@
+<?php 
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+require_once 'init.php';
+include '1.php';
+
+?>
---
+++ school.com/init.php
@@ -0,0 +1,6 @@
+<?php
+//ob_start();
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+date_default_timezone_set("Etc/GMT+3");
+?>
